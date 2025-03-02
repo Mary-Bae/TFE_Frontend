19733a102e1eafb3b18753874228b65293c625a1
Marylène BAEYENS <baeyens.ma@gmail.com>
2025-03-03 00:33:26 +0100

    utilisation avec l'api

--- Frontend/src/app/app.config.ts
+++ Frontend/src/app/app.config.ts
@@ -1,8 +1,8 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-import {provideAuth0, authHttpInterceptorFn} from '@auth0/auth0-angular';
-import { provideHttpClient, withInterceptors} from '@angular/common/http'
+import {provideAuth0} from '@auth0/auth0-angular';
+import { provideHttpClient} from '@angular/common/http'
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -13,26 +13,9 @@ export const appConfig: ApplicationConfig = {
       clientId : 'quhdnv5lsdrgukBLszB9tMzKU7hymnVG',
       authorizationParams : {
         redirect_uri : window.location.origin,
-       
+        audience: 'https://FlexiTimeAPI'
       },
-      httpInterceptor : {
-        allowedList : [
-          {
-            uri: 'http://localhost:5000/api/*',             
-            uriMatcher : (uri) => { 
-              // un exemple d'url qui ne demande pas un access token 
-              return uri != 'http://localhost:5000/api/Values/getall' 
-            }, 
-            tokenOptions: { 
-              authorizationParams: { 
-                audience: 'https://WebApiForAngular-26-11-2024',     
-              } 
-            }
-          }
-        ]
-      }
-      
     }),
-    provideHttpClient(withInterceptors([authHttpInterceptorFn]))
+    provideHttpClient()
   ]
 };
--- Frontend/src/app/dashboard/dashboard.component.ts
+++ Frontend/src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { AuthService } from '@auth0/auth0-angular';
 
 @Component({
   selector: 'app-dashboard',
@@ -8,5 +10,22 @@ import { Component } from '@angular/core';
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
+  response = '';
+constructor(private http : HttpClient, private auth:AuthService){}
 
+callWebApi() {
+  this.response = 'wait for api ...'
+  this.auth.getAccessTokenSilently()
+  .subscribe({
+    next:(token) =>{
+      this.http.get<string>('https://localhost:7290/api/test/GetPrivate',
+        {headers : {Authorization : 'Bearer ' + token}}
+      )
+      .subscribe({
+        next : (v) => this.response = JSON.stringify(v),
+        error : (err) => this.response = 'Error : ' + JSON.stringify(err)
+      });
+    }
+  })
+}
 }
