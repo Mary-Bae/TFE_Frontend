283b1600831af63ba86c6c31971bf8744ac415f5
Marylène BAEYENS <baeyens.ma@gmail.com>
2025-03-02 13:26:33 +0100

    Creation du component dashboard et implémentation du guard sur sa route

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,10 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { AuthGuard } from '@auth0/auth0-angular';
 
 export const routes: Routes = [
     { path: '', component:HomeComponent },
     {path : 'home', component : HomeComponent},
-    {path : 'about', canActivate : [AuthGuard], component : AboutComponent}
+    {path : 'dashboard', canActivate : [AuthGuard], component : DashboardComponent}
 ];
