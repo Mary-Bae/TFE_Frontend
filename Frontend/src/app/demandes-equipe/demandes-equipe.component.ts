28cf6327b32ee6b17e2d7ea4fd733171b3d69b0a
Marylène BAEYENS <baeyens.ma@gmail.com>
2025-04-19 23:06:28 +0200

    Tableau des Demandes de l'équipe implémenté

--- Frontend/src/app/demandes-equipe/demandes-equipe.component.ts
+++ Frontend/src/app/demandes-equipe/demandes-equipe.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DemandesService } from '../shared/demandes.service';
+import { Router } from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-demandes-equipe',
   standalone: true,
-  imports: [],
+  imports: [NgbModule, CommonModule],
   templateUrl: './demandes-equipe.component.html',
   styleUrl: './demandes-equipe.component.css'
 })
 export class DemandesEquipeComponent {
+  demandes: any;
 
+  constructor(private demandesService: DemandesService, private router:Router){
+  }
+  ngOnInit(): void {
+    this.loadDemandes();  }
+
+  loadDemandes() {
+    this.demandesService.GetDemandesEquipe().subscribe(x=>{
+      this.demandes=x
+    })
+  }
 }
