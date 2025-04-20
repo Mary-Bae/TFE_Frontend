c8439603db63a3158e61601db9a25f0c1b532b68
Marylène BAEYENS <baeyens.ma@gmail.com>
2025-04-20 20:34:07 +0200

    Recuperation des infos de l'employé et de la demande validée pour l'envoi du mail d'acceptation et rajout d'un footer

--- Frontend/src/app/shared/employe.service.ts
+++ Frontend/src/app/shared/employe.service.ts
@@ -12,8 +12,10 @@ export class EmployeService {
   constructor(private http: HttpClient) { }
 
   GetMailManagerByUser(): Observable<Employe>{
-  
     return this.http.get<Employe>("https://localhost:7290/Employe/GetMailManagerByUser");
+  }
+  GetMailByDemande(demandeId: number): Observable<Employe>{
+    return this.http.get<Employe>("https://localhost:7290/Employe/GetMailByDemande?demId=" + demandeId);
   }
 }
 
