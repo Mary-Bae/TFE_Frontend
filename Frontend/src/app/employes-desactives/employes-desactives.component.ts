0189a088a283c29594f146843b1c75593b086758
Marylène BAEYENS <baeyens.ma@gmail.com>
2025-06-23 17:49:12 +0200

    rajout de la page de restauration

---
+++ Frontend/src/app/employes-desactives/employes-desactives.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import Swal from 'sweetalert2';
+import { EmployeService } from '../shared/services/employe.service';
+
+@Component({
+  selector: 'app-employes-desactives',
+  standalone: true,
+  imports: [NgbModule, CommonModule],
+  templateUrl: './employes-desactives.component.html',
+  styleUrl: './employes-desactives.component.css'
+})
+export class EmployesDesactivesComponent implements OnInit{
+employes: any;
+
+constructor(private employeService: EmployeService, private router:Router){
+    
+  }
+  ngOnInit(): void {
+    this.loadEmploye(); 
+  }
+  loadEmploye() {
+    this.employeService.GetDeletedUsers().subscribe(x=>{
+      this.employes=x
+    })
+  }
+
+  restore(id: number) {
+    if (confirm('Voulez-vous vraiment restaurer cet utilisateur ?')) {
+      this.employeService.RestoreEmploye(id, this.employes).subscribe(response => {    
+        Swal.fire({
+                  icon: 'success',
+                  title: 'Employé restauré avec succès !',
+                  confirmButtonText: 'OK',
+              });
+        this.loadEmploye();
+      },
+      error => {
+        console.error("Failed to restore user:", error);
+      }
+    );
+      }     
+   }
+
+   navigateToActived(){
+      this.router.navigate(['/employes']);
+
+  }
+  }
+
+
